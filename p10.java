e16d02b6de22f54b222e6715687bf30c3cf1608c
nisargashindhe <124601195+nisargashindhe@users.noreply.github.com>
2023-02-19 12:39:50 +0530

    Create p10.java

---
+++ p10.java
@@ -0,0 +1,26 @@
+10. Write a Java program to demonstrate user defined packageprogram.
+  
+  
+package mypack;
+public class Addition
+{
+public double a,b;
+public Addition(double a,double b)
+{
+this.a=a;
+this.b=b;
+}
+public double sum()
+{
+return (a+b);
+}
+}
+import mypack.Addition;
+public class TestPack
+{
+public static void main(String[] args)
+{
+Addition a=new Addition(50.25,20.60);
+System.out.println("Addition of two numbers::"+a.sum());
+}
+}
